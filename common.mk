7e3877dc5a28cff2e0601b5bf9c98932bb67e871
Pranav Vashi <neobuddy89@gmail.com>
2022-06-25 02:41:51 +0530

    sm8550-common: Enable blur effect
    * crdroid feature
    * Changes QS transparency into blur effect.
    
    Change-Id: Ib2d7c7b893d0f12e2c52f47ae68f7ab28dda63e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -101,6 +101,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
