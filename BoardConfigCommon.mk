db63c18c5e101a4b44f2ee8ffe120c41960f7694
Pranav Vashi <neobuddy89@gmail.com>
2022-12-01 02:27:47 +0530

    sm8550-common: Switch to latest clang for kernel
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,6 +132,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8550
 TARGET_KERNEL_CONFIG := salami_defconfig
+TARGET_KERNEL_CLANG_VERSION := latest
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
