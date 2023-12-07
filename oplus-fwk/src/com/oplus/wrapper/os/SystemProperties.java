634fec1b6149b43f29c6c57b42e35a3f7937da9f
Vishalcj17 <vishalcj@aospa.co>
2023-12-08 05:43:49 +0900

    sm8550-common: oplus-fwk: Update for A14
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- oplus-fwk/src/com/oplus/wrapper/os/SystemProperties.java
+++ oplus-fwk/src/com/oplus/wrapper/os/SystemProperties.java
@@ -1,24 +1,28 @@
 package com.oplus.wrapper.os;
 
+/* loaded from: classes.dex */
 public class SystemProperties {
-
     public static String get(String key) {
-        return SystemProperties.get(key, "");
+        return android.os.SystemProperties.get(key);
     }
 
     public static String get(String key, String def) {
-        return SystemProperties.get(key, def);
-    }
-
-    public static int getInt(String key, int def) {
-        return SystemProperties.getInt(key, def);
+        return android.os.SystemProperties.get(key, def);
     }
 
     public static boolean getBoolean(String key, boolean def) {
-        return SystemProperties.getBoolean(key, def);
+        return android.os.SystemProperties.getBoolean(key, def);
+    }
+
+    public static int getInt(String key, int def) {
+        return android.os.SystemProperties.getInt(key, def);
+    }
+
+    public static long getLong(String key, long def) {
+        return android.os.SystemProperties.getLong(key, def);
     }
 
     public static void set(String key, String val) {
-        SystemProperties.set(key, val);
+        android.os.SystemProperties.set(key, val);
     }
 }
