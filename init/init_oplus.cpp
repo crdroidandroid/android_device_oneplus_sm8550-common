375cecd5f3b48764ff9ff1fce6347fa2f05fe12d
Pranav Vashi <neobuddy89@gmail.com>
2023-12-07 23:57:28 +0530

    sm8550-common: Set marketname for proper device info
    * crdroid feature
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -108,4 +108,5 @@ void vendor_load_properties() {
     set_ro_build_prop("model", model);
     set_ro_build_prop("name", name);
     set_ro_build_prop("product", model, false);
+    set_ro_build_prop("marketname", "OnePlus 11 5G");
 }
