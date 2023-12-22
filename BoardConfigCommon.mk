52d08faa23d940c9f9bb1aaa563fea41a82d673a
Pranav Vashi <neobuddy89@gmail.com>
2023-12-22 19:30:17 +0530

    sm8550-common: Update dependencies for firmware
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,6 +30,32 @@ AB_OTA_PARTITIONS += \
     vendor_boot \
     vendor_dlkm
 
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    oplus_sec \
+    oplusstanvbk \
+    qupfw \
+    shrm \
+    splash \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
