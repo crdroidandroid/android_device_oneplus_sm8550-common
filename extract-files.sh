44e4e830368b5456dee5d3ba70c09c26b4cd7412
Pranav Vashi <neobuddy89@gmail.com>
2024-01-01 01:09:09 +0530

    sm8550-common: Link dolby blobs with v33 libstagefright_foundation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -89,6 +89,11 @@ function blob_fixup() {
         system_ext/etc/camera/mwCalibrationCfg.xml)
             sed -i "s/-----/--/" "${2}"
             ;;
+        vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
+        vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
+        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+            ${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
