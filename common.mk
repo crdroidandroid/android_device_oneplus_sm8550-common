1552624e172dcc54a37f73fd5c1af72b7d31ddd8
Bruno Martins <bgcngm@gmail.com>
2024-09-01 16:42:05 +0100

    sm8550-common: Drop soong configs for audio service
    Not necessary anymore, since the shift to 64bit-only builds.
    
    Change-Id: Ia424deb93e05cfb86706727b62d2ef0f53fa2fb1

--- common.mk
+++ common.mk
@@ -37,11 +37,6 @@ PRODUCT_PACKAGES += \
     tri-state-key-calibrate
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += \
-    run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
-
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
