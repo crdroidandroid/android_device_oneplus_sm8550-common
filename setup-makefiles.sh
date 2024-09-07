6d33b9344775633caa47596c420d11754df96101
Bruno Martins <bgcngm@gmail.com>
2024-09-07 14:35:39 +0100

    sm8550-common: Setup ELF checks
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I05889715e22efa31a39cb131b6a96065337065b3

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,12 +13,91 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
+export TARGET_ENABLE_CHECKELF=true
+
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/oneplus/sm8550-common",
+		"hardware/qcom-caf/sm8550",
+		"hardware/qcom-caf/wlan",
+		"hardware/oplus",
+		"vendor/qcom/opensource/commonsys/display",
+		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/dataservices",
+EOF
+}
+
+function lib_to_package_fixup_odm_variants() {
+    if [ "$2" != "odm" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        libpwirisfeature | \
+            libpwirishalwrapper)
+            echo "$1_odm"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.dpm.api@1.0 | \
+            libQnnCpu | \
+            libQnnHtp | \
+            libQnnHtpPrepare | \
+            libQnnHtpV73Stub | \
+            libhwconfigurationutil | \
+            vendor.oplus.hardware.cammidasservice-V1-ndk | \
+            vendor.oplus.hardware.communicationcenter-V1-ndk | \
+            vendor.oplus.hardware.performance-V1-ndk | \
+            vendor.oplus.hardware.sendextcamcmd-V1-ndk | \
+            vendor.oplus.hardware.stability.oplus_project-V1-ndk | \
+            vendor.pixelworks.hardware.display@1.0 | \
+            vendor.pixelworks.hardware.display@1.1 | \
+            vendor.pixelworks.hardware.display@1.2 | \
+            vendor.pixelworks.hardware.feature@1.0 | \
+            vendor.pixelworks.hardware.feature@1.1 | \
+            vendor.qti.diaghal@1.0 | \
+            vendor.qti.hardware.dpmservice@1.0 | \
+            vendor.qti.hardware.qccsyshal@1.0 | \
+            vendor.qti.hardware.qccsyshal@1.1 | \
+            vendor.qti.hardware.qccsyshal@1.2 | \
+            vendor.qti.hardware.qccvndhal@1.0 | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
+            vendor.qti.imsrtpservice@3.0 | \
+            vendor.qti.imsrtpservice@3.1)
+            echo "$1_vendor"
+            ;;
+        libagmclient | \
+            libpalclient | \
+            libwpa_client) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_odm_variants "$@" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
 
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
